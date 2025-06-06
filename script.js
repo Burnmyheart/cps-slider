56445b47294abd278564d87dd44d6f0d2a33533b
Daniil Zhbir <54086833+Burnmyheart@users.noreply.github.com>
2025-06-06 23:18:58 +0300

    mobile-logo

---
+++ script.js
@@ -0,0 +1,10 @@
+new Swiper('.brands__slider', {
+    slidesPerView: 'auto',
+    slidesOffsetBefore: 0,
+    spaceBetween: 16,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+
+    }
+})
